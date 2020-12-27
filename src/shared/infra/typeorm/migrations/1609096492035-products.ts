f57ea3d52d8409888aae45038563b320fcb24209
Diego Amâncio <diego.pereira@ccc.ufcg.edu.br>
2020-12-27 17:27:37 -0300

    done

---
+++ src/shared/infra/typeorm/migrations/1609096492035-products.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/class-name-casing */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+// eslint-disable-next-line import/prefer-default-export
+export class products1609096492035 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'price',
+            type: 'decimal(10,2)',
+          },
+          {
+            name: 'quantity',
+            type: 'int',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
